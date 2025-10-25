e141b699956a8fe6e9d854352482885bde5baf7e
Seungwon-Choi <bestwondev@gmail.com>
2025-10-25 15:04:50 +0900

    feat: 최종 실행 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.config.AppConfig;
+import racingcar.contoller.RacingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = AppConfig.createRacingController();
+        racingController.run();
     }
 }

49397011d898b86fec55a88a553f8e38a2a09f21
Seungwon-Choi <bestwondev@gmail.com>
2025-10-27 20:58:24 +0900

    refactor: RandomMoveStrategy 상수 추출

--- src/main/java/racingcar/domain/strategy/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/strategy/RandomMoveStrategy.java
@@ -4,9 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
 
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+    private static final int MOVE_THRESHOLD = 4;
+
     // camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 통해 0~9 사이의 숫자를 무작위로 추출한다.
     @Override
     public boolean isMovable() {
-        return Randoms.pickNumberInRange(0, 9) >= 4;
+        return Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX) >= MOVE_THRESHOLD;
     }
 }
